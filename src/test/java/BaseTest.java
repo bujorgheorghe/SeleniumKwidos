841bd9053ce93899237ee8b949b2422850c7db5e
bujorgheorghe <56941435+bujorgheorghe@users.noreply.github.com>
2020-03-13 21:47:01 -0400

    A4 – Homework: Finish Automation tests for Sign Up
    A4 – Homework: Finish Automation tests for Sign Up

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,16 +1,19 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeMethod
     public void setUp(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterMethod
