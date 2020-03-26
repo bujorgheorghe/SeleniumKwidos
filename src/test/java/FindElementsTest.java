9bc834b1d6f0517759b79479e616ee16c990f1bd
bujorgheorghe <56941435+bujorgheorghe@users.noreply.github.com>
2020-03-25 20:03:31 -0400

    A4 – Homework – Lists – Find Elements . – practice
    A4 – Homework – Lists – Find Elements . – practice

---
+++ src/test/java/FindElementsTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class FindElementsTest extends BaseTest{
+    @Test
+    public void numberOfCardsAssert(){
+        driver.get("https://kwidos.tk/offer/search");
+        int num = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("[class='card-header']"))).size();
+        Assert.assertEquals(num,10);
+        }
+
+    @Test
+    public void filterByService(){
+        driver.get("https://kwidos.tk/offer/search");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div:nth-child(2) > p-multiselect"))).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.ui-multiselect-items-wrapper > ul > li:nth-child(1)"))).click();
+        List<WebElement> serviceType= wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("[class*='xs-8 s']")));
+        for (WebElement element:serviceType){
+            Assert.assertEquals(element.getText(),"3D Capture");
+        }
+    }
+}
